86885e11bd0f1c1f75efe03d3255c42ef7446527
dulik <dulikmixail@gmail.com>
2024-10-24 00:51:37 +0300

    part 15-16

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,12 @@
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: {
+        template: '%s | Acme Customers',
+        default: 'Customers',
+    },
+};
+
 export default function Page() {
     return <p>Customers Page</p>;
 }
